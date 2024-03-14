600799b5d47c74529173f299515fc9537722dfb0
Allison <allisonmeier01@gmail.com>
2024-03-13 23:32:44 -0400

    Checkpoint 3

--- Assignment3-Medava/medava-main/src/main/java/edu/uc/cs3003/medava/Thrombolytic.java
+++ Assignment3-Medava/medava-main/src/main/java/edu/uc/cs3003/medava/Thrombolytic.java
@@ -1,1 +1,24 @@
 package edu.uc.cs3003.medava;
+
+// Subclass of Medicine
+public class Thrombolytic extends Medicine {
+
+    // Constructor
+    public Thrombolytic() {
+        super("Thrombolytic");
+    }
+
+    // Methods
+    @Override
+    public double minimumTemperature() {
+      System.out.println("Getting the minimum safe temperature for a Thrombolytic drug.");
+      return 39.2;
+    }
+  
+    @Override
+    public double maximumTemperature() {
+      System.out.println("Getting the maximum safe temperature for a Thrombolytic drug.");
+      return 41.0;
+    }
+
+}
